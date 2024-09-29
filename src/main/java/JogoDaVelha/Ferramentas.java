3ff8374c346c76705a9cda967227e14679008413
jesse <jee_boeira@hotmail.com>
2024-09-29 02:07:09 -0300

    Primeiro Commit v2

--- src/main/java/JogoDaVelha/Ferramentas.java
+++ src/main/java/JogoDaVelha/Ferramentas.java
@@ -5,13 +5,14 @@ import java.util.Scanner;
 public class Ferramentas {
     static Scanner scan = new Scanner(System.in);
 
-    // Ferramenta escolhe time, recebe apenas X ou O ou indica o erro e entra em recursão.
     public static char escolheTime(char time){
         switch (time){
             case 'X': case 'x':
                 return 'X';
+            //break;
             case 'O': case 'o':
                 return 'O';
+            //break;
             default:
                 System.out.println("Opção inválida, escolha X ou O");
                 escolheTime(scan.next().charAt(0));
@@ -19,25 +20,9 @@ public class Ferramentas {
         return 0;
     }
 
-    public static void CriaJogadores() {
-        //Coleta o nome do Jogador 1
-        System.out.println("Nome do Jogador 1: ");
-        String nome1 = scan.next();
-
-        //Chama a ferramenta escolhe time, que recebe apenas X ou O.
-        System.out.println("Escolha o time (X/O): ");
-        char time1 = Ferramentas.escolheTime(scan.next().charAt(0));
-
-        //Coleta o nome do Jogador 2
-        System.out.println("Nome do Jogador 2: ");
-        String nome2 = scan.next();
-
-        //Realiza operação ternária para verificar o time restante
-        char time2 = (time1 == 'X') ? 'O' : 'X';
-        System.out.printf("Seu time é o: %s%n", time2);
-
-        //Cria os jogadores
-        Jogador j1 = new Jogador(nome1, time1);
-        Jogador j2 = new Jogador(nome2, time2);
+    public static boolean desejaContinuar() {
+        System.out.println("Deseja jogar novamente? (s/n)");
+        String resposta = scan.next();
+        return resposta.equalsIgnoreCase("s");
     }
 }
--- src/main/java/JogoDaVelha/Main.java
+++ src/main/java/JogoDaVelha/Main.java
@@ -1,5 +1,4 @@
 package JogoDaVelha;
-ext
 
 import java.util.Scanner;
 
@@ -7,11 +6,24 @@ public class Main {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
+
         System.out.println("Bem-vindo ao Jogo da Velha!");
 
-        // Recebe os nomes e times dos jogadores
-        Ferramentas.CriaJogadores();
-        // Inicia um jogo
+        // Coleta nomes e times dos jogadores
+        System.out.print("Nome do jogador 1: ");
+        String nome1 = scan.nextLine();
+        System.out.print("Escolha o time (X/O): ");
+        //chama a ferramenta escolhe time, para receber apenas X ou O.
+        char time1 = Ferramentas.escolheTime(scan.next().charAt(0));
+
+        System.out.print("Nome do jogador 2: ");
+        String nome2 = scan.next();
+        char time2 = (time1 == 'X') ? 'O' : 'X';
+        System.out.printf("Seu time é o: %s%n", time2);
+
+        //Cria os jogadores e inicia uma partida
+        Jogador j1 = new Jogador(nome1, time1);
+        Jogador j2 = new Jogador(nome2, time2);
         Jogo jogo = new Jogo();
 
         boolean continuar = true;
