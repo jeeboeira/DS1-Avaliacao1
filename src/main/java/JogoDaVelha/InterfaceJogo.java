a31b7650857a7df11248cdf9c3f7a2bdb2056780
jesse <jee_boeira@hotmail.com>
2024-09-29 22:11:53 -0300

    Código Finalizado e limpo! :tada:

--- src/main/java/JogoDaVelha/InterfaceJogo.java
+++ src/main/java/JogoDaVelha/InterfaceJogo.java
@@ -1,9 +1,9 @@
 package JogoDaVelha;
 
 public interface InterfaceJogo {
-    public Jogador temVencedor  (Jogador j1, Jogador j2);
-    public void    novoJogo     ();
-    public void    atualizaTela ();
-    public boolean ehEmpate     ();
-    public boolean ehFimDoJogo  ();
+    Jogador temVencedor  (Jogador j1, Jogador j2);
+    void    novoJogo     ();
+    void    atualizaTela ();
+    boolean ehEmpate     ();
+    boolean ehFimDoJogo  ();
 }
